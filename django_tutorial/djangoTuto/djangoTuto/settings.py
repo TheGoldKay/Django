f98349edeee42383fd4c239547dd324831f78322
Jhonnatan Carvalho <jhonnatanwfc@live.com>
2023-12-19 20:44:40 -0300

    styling django templates

--- django_tutorial/djangoTuto/djangoTuto/settings.py
+++ django_tutorial/djangoTuto/djangoTuto/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'polls',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'djangoTuto.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'polls', 'templates')],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,7 +117,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+# why must I specify where exactly is the static folders for the specific app?
+STATIC_URL = os.path.join(BASE_DIR, '/polls/static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
