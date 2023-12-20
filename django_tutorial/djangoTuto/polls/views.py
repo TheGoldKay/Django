7bf1488aae8d44d5885965e3e647082945066fb0
Jhonnatan Carvalho <jhonnatanwfc@live.com>
2023-12-20 07:44:57 -0300

    another path

--- django_tutorial/djangoTuto/polls/views.py
+++ django_tutorial/djangoTuto/polls/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, "index.html")
+
+def home(request):
+    return HttpResponse(f"{str(request)} - > You're at the homepage: Got to /polls/")
